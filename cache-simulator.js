0a390e9555301fb45d743c58868471b742e48189
Joana Mespaque <joanamespaque@gmail.com>
2023-04-08 21:32:30 -0300

    read decimal values from binary files

--- cache-simulator.js
+++ cache-simulator.js
@@ -1,6 +1,17 @@
 import { argv } from "node:process";
+import { Buffer } from "node:buffer";
+import * as fs from "node:fs";
 
 // print process.argv
 argv.forEach((val, index) => {
   console.log(`${index}: ${val}`);
 });
+
+try {
+  const data = fs.readFileSync("./addresses/bin_10000.bin");
+  for (let i = 0; i <= data.length - 4; i += 4) {
+    console.log(data.readInt32BE(i));
+  }
+} catch (error) {
+  console.error(error);
+}

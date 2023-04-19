eb151489fbc7a598e69c6490f7a9b15f477d5ccd
Gabriela Tomaz <gabrielatomaz1772@gmail.com>
2023-04-18 21:15:22 -0300

    added final print

--- cache_simulator.js
+++ cache_simulator.js
@@ -21,6 +21,7 @@ try {
   } else if (argv[6] !== "1" && argv[6] !== "0") {
     throw new Error("Verifique o modo de saída passado como argumento.");
   }
+
   const info = {
     nsets: argv[2],
     bsize: argv[3],
@@ -39,40 +40,34 @@ try {
 
   const n_access = addresses.length;
 
-  // TODO: #4 #3 calcular tamanho da cache
-  // O tamanho da cache é dado pelo produto do número de conjuntos na cache (<nsets>), tamanho do bloco em bytes (<bsize>) e associatividade (<assoc>).
-  const cache_size = info.nsets * info.bsize * info.assoc;
-  // TODO: #5 criar cache como uma matriz, dependendo da associatividade
-  let cache = [];
+  const cache = [];
   // adiciona conjuntos a cache
   for (let n_set = 0; n_set < info.nsets; n_set++) {
-    let set = [];
+    const set = [];
     for (let i = 0; i < info.assoc; i++) {
       // adiciona os blocos de acordo com a associatividade
       set.push({ val: 0, tag: null });
     }
     cache.push(set);
   }
+
   const n_bits_offset = Math.log2(info.bsize);
   const n_bits_indice = Math.log2(info.nsets);
-  const n_bits_tag = 32 - n_bits_indice - n_bits_offset;
-  // TODO: #6 calcular taxa de misses compulsórios, de capacidade e conflito
+
   let n_compulsory_miss = 0;
   let n_capacity_miss = 0;
   let n_conflict_miss = 0;
   let n_hits = 0;
 
   addresses.forEach((item) => {
-    let tag = item >> parseInt(n_bits_offset + n_bits_indice);
-    let indice =
-      (item >> parseInt(n_bits_offset)) &
+    const tag = item >> parseInt(n_bits_offset + n_bits_indice);
+    const indice = (item >> parseInt(n_bits_offset)) &
       parseInt(Math.pow(2, n_bits_indice) - 1);
-    let index = indice % info.nsets;
+    const index = indice % info.nsets;
     // para mapeamento direto:
     if (info.assoc == 1) {
       // na posição 0 bit de validade, posição 1 tag para comparação
-      let current = cache[index][0];
-      console.log(current);
+      const current = cache[index][0];
       if (current.val === 0) {
         n_compulsory_miss++;
         cache[index][0] = { val: 1, tag };
@@ -86,25 +81,24 @@ try {
       }
     } else {
       // para totalmente associativo
-      let cache_set = info.nsets == 1 ? 0 : index;
+      const cache_set = info.nsets == 1 ? 0 : index;
 
-      let fill = false;
-
+      let full = false;
       cache[cache_set].every((item, i) => {
         if (item.val == 0) {
           n_compulsory_miss++;
           cache[cache_set][i] = { val: 1, tag };
-          fill = true;
+          full = true;
           return false;
         } else if (item.tag === tag) {
           n_hits++;
-          fill = true;
+          full = true;
           return false;
         }
         return true;
       });
 
-      if (!fill) {
+      if (!full) {
         // capacidade ou conflito?
         if (info.nsets != 1) {
           // calcularei misses de conflito
@@ -113,8 +107,9 @@ try {
             set.forEach((block) => {
               block.val === 0 && count_empty++;
             });
-            return count_empty !== 0 ? false : true;
+            return !(count_empty !== 0);
           });
+
           if (count_empty !== 0) {
             n_conflict_miss++;
           } else {
@@ -122,26 +117,39 @@ try {
           }
         } else {
           // cache totalmente associativa, n_conflict_misses === 0
-          console.log("miss capacidade");
           n_capacity_miss++;
         }
 
-        let random_index = parseInt(Math.random() * parseInt(info.assoc));
+        const random_index = parseInt(Math.random() * parseInt(info.assoc));
         cache[cache_set][random_index].tag = tag;
       }
     }
   });
 
   const misses = n_capacity_miss + n_compulsory_miss + n_conflict_miss;
-  console.log("acessos: ", n_access);
-  console.log("hit rate: ", n_hits / n_access);
-  console.log("miss rate: ", misses / n_access);
-  console.log("n_compulsory_miss: ", n_compulsory_miss / misses);
-  console.log("n_capacity_miss: ", n_capacity_miss / misses);
-  console.log("n_conflict_miss: ", n_conflict_miss / misses);
+  const hit_rate = (n_hits / n_access).toFixed(4);
+  const miss_rate = (misses / n_access).toFixed(4);
+  const n_compulsory_miss_rate = (n_compulsory_miss / misses).toFixed(2);
+  const n_capacity_miss_rate = (n_capacity_miss / misses).toFixed(2);
+  const n_conflict_miss_rate = (n_conflict_miss / misses).toFixed(2);
 
-  // como ler cada um dos endereços para comparação:
-  // TODO: #7 calcular taxa de hits
+  const print_message = {
+    0: `acessos: ${n_access}\n`
+      + `hit rate: ${hit_rate}\n`
+      + `miss rate: ${miss_rate}\n`
+      + `compulsory miss: ${n_compulsory_miss_rate}\n`
+      + `capacity miss: ${n_capacity_miss_rate}\n`
+      + `conflict miss: ${n_conflict_miss_rate}`,
+    1: `${n_access} `
+      + `${hit_rate} `
+      + `${miss_rate} `
+      + `${n_compulsory_miss_rate} `
+      + `${n_capacity_miss_rate} `
+      + `${n_conflict_miss_rate}`
+  };
+
+  console.log(print_message[info.out_flag]);
+
 } catch ({ message, code }) {
   console.error("Erro encontrado: ", message);
   // verificar se a linha de comando tem o total correto de argumentos:
